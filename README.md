62092142ba2883e150b61bf2aa667d0e93d3a4db
Caviar-X <caviarx@163.com>
2023-09-02 11:24:08 +0800

    add tweet system

--- README.md
+++ README.md
@@ -4,16 +4,24 @@
 
 可能是已知的第一个第三方主题。
 
-不管你的审美是啥，我喜欢老电脑。
-
 ## 如何使用？
 
 1. 去[这里](http://www.telihai.com/archives/9027/)下载beex
 2. 将本仓库clone至_themes文件夹
 3. 在`config.toml`里的themes一栏改为`beex-386`
 4. 编辑`main.lua`文件中的全局变量`config`
-5. 愉快的开始写作吧
+5. 将favicon.ico放入static🗂中
+6. 愉快的开始写作吧
+
+## Tweet
+
+请在`./plugin/main.lua`中的`enable_tweet`字段设置为true
+
+beex_meta中的tags第一位必须设置为`tweet`
 
 ## 目前bug/TODOs
 
 - 由于带了个点阵宋体，主题臃肿。尝试削减主题大小
+
+- 加入tag.html
+
--- plugin/main.lua
+++ plugin/main.lua
@@ -1,5 +1,6 @@
 --[[
     请在此编辑你的喜好设置
+    网站icon请将favicon.ico放入网站的static文件夹中
 ]]
 local config = {
     --站点开始运行的年份
@@ -14,9 +15,10 @@ local config = {
     magic_cursor = true,
 
     --prism.js主题名，具体可以参照prismjs.com 默认主题留空即可,
-    prism_theme = ''
+    prism_theme = '',
 
-    --网站icon请将favicon.ico放入网站的static文件夹中
+    --是否开启tweet,详见README.md
+    enable_tweet = true,
 }
 
 
@@ -25,7 +27,8 @@ function Theme_context(args)
         magic_cursor = config.magic_cursor,
         custom_head = config.custom_head,
         begin_year = config.begin_year,
-        prism_theme = config.prism_theme
+        prism_theme = config.prism_theme,
+        enable_tweet = config.enable_tweet
     }
     return args
 end
